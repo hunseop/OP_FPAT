fb5a826d9bf04066ea78dfa879387da8ddeacae2
hunseop <50357363+hunseop@users.noreply.github.com>
2025-01-10 17:29:19 +0900

    gui 프로젝트 시작준비

--- modules/paloalto_api_v2.py
+++ modules/paloalto_api_v2.py
@@ -4,29 +4,27 @@ requests.packages.urllib3.disable_warnings()
 import xml.etree.ElementTree as ET
 import pandas as pd
 import logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 import time
 import os
 import datetime
-
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 def apply_excel_style(file_name):
     """
-        주어진 엑셀파일의 헤더에 연한 회색 배경을 적용하고,
-        헤더의 너비를 자동으로 조절하되 최대 너비를 40으로 제한하는 함수.
+    주어진 엑셀파일의 헤더에 연한 회색 배경을 적용하고,
+    헤더의 너비를 자동으로 조절하되 최대 너비를 40으로 제한하는 함수.
 
-        :param file_name: 처리할 엑셀파일의 이름
+    :param file_name: 처리할 엑셀파일의 이름
     """
     try:
-        # load excel file
         workbook = load_workbook(file_name)
         worksheet = workbook.active
 
-        # set range of header (first row is header)
+        # Set range of header (first row is header)
         header_range = worksheet[1]
-
         fill = PatternFill(start_color='D3D3D3', end_color='D3D3D3', fill_type='solid')
 
         for cell in header_range:
@@ -45,7 +43,7 @@ def apply_excel_style(file_name):
         workbook.save(file_name)
     
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logging.error(f"An error occurred: {e}")
 
 class PaloAltoAPI:
     def __init__(self, hostname, username, password):
@@ -54,6 +52,9 @@ class PaloAltoAPI:
         self.api_key = self.get_api_key(username, password)
 
     def save_dfs_to_excel(self, dfs, sheet_names, file_name):
+        """
+        여러 DataFrame을 엑셀 파일로 저장하고 스타일을 적용하는 함수
+        """
         try:
             if not isinstance(dfs, list):
                 dfs = [dfs]
@@ -66,10 +67,14 @@ class PaloAltoAPI:
                     df.to_excel(writer, sheet_name=sheet_name, index=False)
             apply_excel_style(file_name)
             return True
-        except:
+        except Exception as e:
+            logging.error(f"An error occurred while saving DataFrames to Excel: {e}")
             return False
     
     def save_df_to_excel(self, df, df_name):
+        """
+        단일 DataFrame을 엑셀 파일로 저장하는 함수
+        """
         current_date = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
         save_file = f"{current_date}_{self.hostname}_{df_name}.xlsx"
         if os.path.isfile(save_file):
@@ -79,84 +84,103 @@ class PaloAltoAPI:
             df.to_excel(writer, sheet_name=df_name, index=False)
     
     def get_member(self, entry):
+        """
+        XML entry에서 member 요소를 추출하는 함수
+
+        :param entry: XML entry
+        :return: member 요소의 텍스트 리스트
+        """
         try:
-            result = [ member.text for member in entry ]
-        except:
-            result = []
-        
-        return result
-    
+            return [member.text for member in entry]
+        except Exception as e:
+            logging.error(f"An error occurred while getting members: {e}")
+            return []
+
     def list_to_string(self, list_data):
+        """
+        리스트를 콤마로 구분된 문자열로 변환하는 함수
+
+        :param list_data: 리스트 데이터
+        :return: 콤마로 구분된 문자열
+        """
         return ','.join(str(s) for s in list_data)
     
     def get_api_data(self, parameter: dict, time_out: int = 10000):
+        """
+        API 요청을 보내고 응답을 반환하는 함수
+        """
         try:
             response = requests.get(self.base_url, params=parameter, verify=False, timeout=time_out)
             return response
-        
         except requests.exceptions.RequestException as e:
-            raise ValueError(f"API 요청 중 요류 발생: {e}")
+            raise ValueError(f"API 요청 중 오류 발생: {e}")
     
-    def get_api_key(self, username:str, password: str):
-        keygen_parameter = (
-            ('type', 'keygen'),
-            ('user', username),
-            ('password', password)
-        )
+    def get_api_key(self, username: str, password: str):
+        """
+        API 키를 가져오는 함수
+        """
+        keygen_parameter = {
+            'type': 'keygen',
+            'user': username,
+            'password': password
+        }
 
         try:
             response = self.get_api_data(keygen_parameter)
             key_value = ET.fromstring(response.text).find('./result/key')
-            api_key = key_value.text
-            
-            return api_key
-        
+            return key_value.text
         except requests.exceptions.RequestException as e:
             raise ValueError(f"API 요청 중 오류 발생: {e}")
     
     def get_vsys_list(self):
-        parameter = (
-            ('key', self.api_key),
-            ('type', 'config'),
-            ('action', 'get'),
-            ('xpath', '/config/devices/entry/vsys/entry'),
-        )
+        """
+        vsys 목록을 가져오는 함수
+        """
+        parameter = {
+            'key': self.api_key,
+            'type': 'config',
+            'action': 'get',
+            'xpath': '/config/devices/entry/vsys/entry'
+        }
 
         response = self.get_api_data(parameter)
         root = ET.fromstring(response.text).findall('./result/entry')
-        vsys_list = []
-        for vsys in root:
-            vsys_list.append(vsys.attrib.get('name'))
-
-        return vsys_list
+        return [vsys.attrib.get('name') for vsys in root]
     
     def get_config(self, config_type: str = 'running'):
+        """
+        설정을 가져오는 함수
+        """
         action = 'show' if config_type == 'running' else 'get'
-        parameter = (
-            ('key', self.api_key),
-            ('type', 'config'),
-            ('action', action),
-            ('xpath', '/config')
-        )
+        parameter = {
+            'key': self.api_key,
+            'type': 'config',
+            'action': action,
+            'xpath': '/config'
+        }
 
-        response = self.get_api.data(parameter)
-
+        response = self.get_api_data(parameter)
         return response.text
     
     def save_config(self, config_type: str = 'running'):
+        """
+        설정을 파일로 저장하는 함수
+        """
         current_date = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
         config = self.get_config(config_type)
         with open(f'{current_date}_{self.hostname}_{config_type}_config.xml', mode='w', encoding='utf8') as file:
             file.write(config)
-        
         return True
     
     def get_system_info(self):
-        parameter = (
-            ('type', 'op'),
-            ('cmd', '<show><system><info/></system></show>'),
-            ('key', self.api_key)
-        )
+        """
+        시스템 정보를 가져오는 함수
+        """
+        parameter = {
+            'type': 'op',
+            'cmd': '<show><system><info/></system></show>',
+            'key': self.api_key
+        }
         response = self.get_api_data(parameter)
 
         tree = ET.fromstring(response.text)
@@ -174,39 +198,38 @@ class PaloAltoAPI:
         return pd.DataFrame(info, index=[0])
     
     def get_system_state(self):
-        parameter = (
-            ('type', 'op'),
-            ('cmd', '<show><system><state><filter>cfg.general.max*</filter></state></system></show>'),
-            ('key', self.api_key)
-        )
+        """
+        시스템 상태를 가져오는 함수
+        """
+        parameter = {
+            'type': 'op',
+            'cmd': '<show><system><state><filter>cfg.general.max*</filter></state></system></show>',
+            'key': self.api_key
+        }
         response = self.get_api_data(parameter)
         tree = ET.fromstring(response.text)
 
         result = tree.findtext("./result")
+        state = {}
         for data in result.split('\n'):
             if data.startswith('cfg.general.max-address:'):
-                max_address = data.split(': ')[1]
+                state["max_address"] = data.split(': ')[1]
             elif data.startswith('cfg.general.max-address-group:'):
-                max_address_group = data.split(': ')[1]
+                state["max_address_group"] = data.split(': ')[1]
             elif data.startswith('cfg.general.max-service:'):
-                max_service = data.split(': ')[1]
+                state["max_service"] = data.split(': ')[1]
             elif data.startswith('cfg.general.max-service-group:'):
-                max_service_group = data.split(': ')[1]
+                state["max_service_group"] = data.split(': ')[1]
             elif data.startswith('cfg.general.max-policy-rule:'):
-                max_policy_rule = data.split(': ')[1]
+                state["max_policy_rule"] = data.split(': ')[1]
         
-        state = {
-            "hostname": self.hostname,
-            "max_policy_rule": max_policy_rule,
-            "max_address": max_address,
-            "max_address_group": max_address_group,
-            "max_service": max_service,
-            "max_service_group": max_service_group
-        }
-
+        state["hostname"] = self.hostname
         return pd.DataFrame(state, index=[0])
     
     def export_security_rules(self, config_type: str = 'running'):
+        """
+        정책을 추출하는 함수
+        """
         config = self.get_config(config_type)
         tree = ET.fromstring(config)
         vsys_list = tree.findall('./result/config/devices/entry/vsys/entry')
@@ -228,12 +251,11 @@ class PaloAltoAPI:
                 url_filtering = self.list_to_string(self.get_member(rule.findall('./profile-setting/profiles/url-filtering/member')))
                 category = self.list_to_string(self.get_member(rule.findall('./category/member')))
                 category = "any" if category == "" else category
-                description = self.get_member(rule.findall('./description'))
-                description = self.list_to_string([ i.replace('\n',' ') if type(i) is str else i for i in description ])
+                description = self.list_to_string([i.replace('\n', ' ') if isinstance(i, str) else i for i in self.get_member(rule.findall('./description'))])
 
                 rule_info = {
                     "Vsys": vsys_name,
-                    "Seq": idx+1,
+                    "Seq": idx + 1,
                     "Rule Name": rule_name,
                     "Enable": disabled,
                     "Action": action,
@@ -251,19 +273,18 @@ class PaloAltoAPI:
         return pd.DataFrame(security_rules)
     
     def export_network_objects(self, config_type: str = 'running'):
+        """
+        네트워크 객체를 추출하는 함수
+        """
         config = self.get_config(config_type)
         tree = ET.fromstring(config)
         address_list = tree.findall('./result/config/devices/entry/vsys/entry/address/entry')
         address_objects = []
         for address in address_list:
-            ip = address.attrib.get('name')
-            ip_name = ip
-            ip_member = []
+            ip_name = address.attrib.get('name')
             address_type = address.find('*').tag
-            ip_entry = address.findall(f'./{address_type}')
-            for j in ip_entry:
-                ip_member.append(j.text)
-            
+            ip_member = [j.text for j in address.findall(f'./{address_type}')]
+
             objects_info = {
                 "Name": ip_name,
                 "Type": address_type,
@@ -274,18 +295,16 @@ class PaloAltoAPI:
         return pd.DataFrame(address_objects)
     
     def export_network_group_objects(self, config_type: str = 'running'):
+        """
+        네트워크 그룹 객체를 추출하는 함수
+        """
         config = self.get_config(config_type)
         tree = ET.fromstring(config)
         address_group_list = tree.findall('./result/config/devices/entry/vsys/entry/address-group/entry')
         address_group_objects = []
         for address_group in address_group_list:
-            group = address_group.attrib.get('name')
-            group_name = group
-            group_member = []
-            
-            group_entry = address_group.findall('./static/member')
-            for j in group_entry:
-                group_member.append(j.text)
+            group_name = address_group.attrib.get('name')
+            group_member = [j.text for j in address_group.findall('./static/member')]
 
             objects_info = {
                 "Group Name": group_name,
@@ -296,13 +315,15 @@ class PaloAltoAPI:
         return pd.DataFrame(address_group_objects)
     
     def export_service_objects(self, config_type: str = 'running'):
+        """
+        서비스 객체를 추출하는 함수
+        """
         config = self.get_config(config_type)
         tree = ET.fromstring(config)
         services = tree.findall('./result/config/devices/entry/vsys/entry/service/entry')
         service_objects = []
         for service in services:
-            service_info = service.attrib.get('name')
-            service_name = service_info
+            service_name = service.attrib.get('name')
 
             for protocol in service.find('protocol'):
                 protocol_name = protocol.tag
@@ -318,34 +339,36 @@ class PaloAltoAPI:
         return pd.DataFrame(service_objects)
     
     def export_service_group_objects(self, config_type: str = 'running'):
+        """
+        서비스 그룹 객체를 추출하는 함수
+        """
         config = self.get_config(config_type)
         tree = ET.fromstring(config)
         service_groups = tree.findall('./result/config/devices/entry/vsys/entry/service-group/entry')
         service_group_objects = []
         for service_group in service_groups:
             group_name = service_group.attrib.get('name')
-            group_member = []
+            group_member = [j.text for j in service_group.findall('./members/member')]
 
-            group_entry = service_group.findall('./members/member')
-            for j in group_entry:
-                group_member.append(j.text)
-            
             service_info = {
                 "Group Name": group_name,
                 "Entry": self.list_to_string(group_member),
             }
             service_group_objects.append(service_info)
-
+        
         return pd.DataFrame(service_group_objects)
     
     def export_hit_count(self, vsys_name: str = 'vsys1'):
-        parameter = (
-            ('type', 'op'),
-            ('cmd', f"<show><rule-hit-count><vsys><vsys-name><entry name='{vsys_name}'><rule-base><entry name='security'><rules><all/></rules></entry></rule-base></entry></vsys-name></vsys></rule-hit-count></show>"),
-            ('key', self.api_key)
-        )
+        """
+        히트 카운트를 추출하는 함수
+        """
+        parameter = {
+            'type': 'op',
+            'cmd': f"<show><rule-hit-count><vsys><vsys-name><entry name='{vsys_name}'><rule-base><entry name='security'><rules><all/></rules></entry></rule-base></entry></vsys-name></vsys></rule-hit-count></show>",
+            'key': self.api_key
+        }
 
-        response = self.get_api.data(parameter)
+        response = self.get_api_data(parameter)
         tree = ET.fromstring(response.text)
         rules = tree.findall('./result/rule-hit-count/vsys/entry/rule-base/entry/rules/entry')
 
